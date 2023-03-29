8d93903b5ecd98eba18c6e9df984b22472be7f06
MartindeOrta <82926690+MartindeOrta@users.noreply.github.com>
2023-03-29 13:51:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,26 @@
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
-string=`{"name": "Hook TOMOUSDT", "secret": "uyccbu6wly8","side": "buy", "symbol": "{{TOMOUSDT}}","open": {"amountType": "balance", "amount": "60","leverage": "2"}}`
-jsondatos={
-    "name": "Hook TOMOUSDT",
-    "secret": "uyccbu6wly8",
-    "side": "buy",
-    "symbol": "{{TOMOUSDT}}",
-    "open": {
-      "amountType": "balance",
-      "amount": "60",
-      "leverage": "2"
-    }
+var raw = JSON.stringify({
+  "name": "Hook TOMOUSDT",
+  "secret": "uyccbu6wly8",
+  "side": "buy",
+  "symbol": "{{TOMOUSDT}}",
+  "open": {
+    "amountType": "balance",
+    "amount": "60",
+    "leverage": "2"
   }
+});
 
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
 
-entrar=document.getElementById('entrar')
-url = document.getElementById('hookId').value;
-data  = document.getElementById('hookBody').value
-// jsondato=JSON.parse(data)
-console.log(data)
-entrar.addEventListener('click', function(){
-    console.log(data)
-    url = "https://hook.finandy.com/PggoHJobbil5v5O6qFUK";
-    console.log(url)
-    data  = jsondatos
-
-
-    jsondatos={
-        "name": "Hook TOMOUSDT",
-        "secret": "uyccbu6wly8",
-        "side": "buy",
-        "symbol": "{{TOMOUSDT}}",
-        "open": {
-          "amountType": "balance",
-          "amount": "60",
-          "leverage": "2"
-        }
-      }
-
-
-
-
-fetch(url, {
-    method: 'POST', // or 'PUT'
-    body: jsondatos, // data can be `string` or {object}!
-    mode: 'cors',
-    headers:{
-      'Content-Type': 'application/json'
-    }
-  })
-.then(dato => function(){datos.json()})
-.then(datos=> console.log(datos))
-.catch(e => function(){console.log(e)})
-}
-  )
+fetch("https://hook.finandy.com/PggoHJobbil5v5O6qFUK", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
