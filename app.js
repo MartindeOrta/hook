8954df9f3d12cef74efd176f2ba6b81e69f0d3ad
MartindeOrta <82926690+MartindeOrta@users.noreply.github.com>
2023-03-29 02:44:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-function formSubmit(e) {
-    const XHR = new XMLHttpRequest(),
-        id = document.getElementById('hookId').value,
-        body = document.getElementById('hookBody').value,
-        response = document.getElementById('hookResponse');
 
-    e.preventDefault();
+   
+                <td><input type="text" id="hookId" style="width: 100%" placeholder="Hook ID"></td>
+          
+              
+              
+                    <textarea id="hookBody" ></textarea>
+                    
+                    <button id="entrar">entrar</button>
+                </td>
+            </tr>
+        </tbody></table>
+    </form>
 
-
-    XHR.open( 'POST',id ,true);
-    XHR.setRequestHeader( 'Content-Type', 'application/json' );
-    XHR.withCredentials = true;
-    XHR.send( body );
-}
-
-document.getElementById('testForm').addEventListener('submit', formSubmit);
+    <script src="app.js"></script>
+</body>
+</html>
